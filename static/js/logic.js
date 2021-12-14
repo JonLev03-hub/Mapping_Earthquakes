98e6b743b74fe8fee60937f28fa8ffdd98a50a9e
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-14 12:39:29 -0500

    added city locations with markers and circles, size is perportional to population

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,6 +11,12 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437]).addTo(map);
-
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location,{radius : city.population/100000,color : "orange"})
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
