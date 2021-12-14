96674209013d86926810394454a0af3026ae451f
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-14 13:37:59 -0500

    tested different geometries

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,5 +1,5 @@
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -22,12 +22,34 @@ cityData.forEach(function(city) {
 });
 
 // Create a polyline using the line coordinates and make the line red.
-let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790],
-    [40.7899, -111.9791],
-    [47.4502, -122.3088]
-  ];
-  L.polyline(line, {
-    color: "yellow"
- }).addTo(map);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(feature);
+      console.log("gotit")
+      return L.marker(latlng)
+      .bindPopup("<h2>" + feature.properties.city + "</h2>"+"<p> random text <p>");
+    }
+
+  }).addTo(map);
