0cc0c99bed04ed158975f6cc3b189710a214aad7
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-18 02:10:42 -0500

    added data

--- static/js/logic.js
+++ static/js/logic.js
@@ -7,7 +7,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,21 +16,19 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   light: light,
-  Dark: dark
+  Satelite: sateliteStreets
 };
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-  center: [40.7, -94.5],
-  zoom: 4
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers:[sateliteStreets]
 });
-
-
-light.addTo(map)
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);  
 
-let torontoData = "https://raw.githubusercontent.com/jonlev03-hub/Mapping_Earthquakes/main/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/jonlev03-hub/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 
 let myStyle = {
@@ -41,5 +39,5 @@ let myStyle = {
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data,{style : myStyle}).addTo(map);
+L.geoJSON(data).addTo(map);
 }).addTo(map)
