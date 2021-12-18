fd2dfb5a5ad0779ad0a00c5b7bb4b3c7b7694d3f
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-18 01:43:04 -0500

    Tested styling

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,7 +15,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
+  light: light,
   Dark: dark
 };
 
@@ -25,5 +25,21 @@ let map = L.map("mapid", {
   zoom: 4
 });
 
+
+light.addTo(map)
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);  
+
+let torontoData = "https://raw.githubusercontent.com/jonlev03-hub/Mapping_Earthquakes/main/torontoRoutes.json";
+
+
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data,{style : myStyle}).addTo(map);
+}).addTo(map)
