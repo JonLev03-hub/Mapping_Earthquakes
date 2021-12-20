54231d3d17a852db84f2cd455ba6e52f4d80301c
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-20 12:41:59 -0500

    added a ledgent to show earthquake magnitude

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -88,7 +88,29 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
         ).addTo(earthquakes)
     });
-  let myStyle = {
-    color: "#ffffa1",
-    weight: 2
-    }
+    let legend = L.control({
+      position: "bottomright"
+    });
+
+    legend.onAdd = function() {
+      let div = L.DomUtil.create("div", "info legend");
+      const magnitudes = [0, 1, 2, 3, 4, 5];
+     const colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "#ea822c",
+  "#ea2c2c"
+];
+    // Looping through our intervals to generate a label with a colored square for each interval.
+   for (var i = 0; i < magnitudes.length; i++) {
+    console.log(colors[i]);
+    div.innerHTML +=
+      "<i style='background: " + colors[i] + "'></i> " +
+      magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+ }
+  return div;
+};
+
+legend.addTo(map);
