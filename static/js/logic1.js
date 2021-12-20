fb51ef416a30a43984a25a88e1aa6efd9264390d
Jonathan Leverenz <jonlev03@gmail.com>
2021-12-19 20:58:49 -0500

    added color coating and popups

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -34,7 +34,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
           opacity: 1,
           fillOpacity: 1,
-          fillColor: "#ffae42",
+          fillColor: getColor(feature.properties.mag),
           color: "#000000",
           radius: getRadius(feature.properties.mag),
           stroke: true,
@@ -47,6 +47,24 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
         return magnitude * 4;
       }
+      function getColor(magnitude) {
+        if (magnitude > 5) {
+          return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+          return "#ea822c";
+        }
+        if (magnitude > 3) {
+          return "#ee9c00";
+        }
+        if (magnitude > 2) {
+          return "#eecc00";
+        }
+        if (magnitude > 1) {
+          return "#d4ee00";
+        }
+        return "#98ee00";
+      }
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJSON(data, {
 
@@ -57,8 +75,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 return L.circleMarker(latlng);
             },
           // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
-        }).addTo(map);
+        style: styleInfo,
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
+        }
+        ).addTo(map)
     });
   let myStyle = {
     color: "#ffffa1",
